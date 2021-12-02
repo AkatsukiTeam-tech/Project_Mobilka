182b802ffe3cac3198806f72ce8d5e6001c95aef
Sociopath <sultan.narys2709@gmail.com>
2021-12-03 00:00:39 +0600

    change Films column

--- app/src/main/java/com/example/project/Entities/Films.java
+++ app/src/main/java/com/example/project/Entities/Films.java
@@ -6,7 +6,11 @@ import java.util.List;
 
 public class Films {
     private Long film_id;
-    private String film_name;
+    private String film_ru_name;
+    private String film_orig_name;
+    private String image_url;
+    private String description;
+    private int restriction;
     private Time film_duration;
     private Date film_date;
     private List<Countries> countries;
@@ -14,14 +18,24 @@ public class Films {
     private List<Genres> genres;
     private List<Cinemas> cinemas;
 
-    public Films() {
-    }
-
-    public Films(Long film_id, String film_name, Time film_duration, Date film_date) {
+    public Films(Long film_id, String film_ru_name, String film_orig_name, String image_url, String description,
+                 int restriction, Time film_duration, Date film_date, List<Countries> countries,
+                 List<Directors> directors, List<Genres> genres, List<Cinemas> cinemas) {
         this.film_id = film_id;
-        this.film_name = film_name;
+        this.film_ru_name = film_ru_name;
+        this.film_orig_name = film_orig_name;
+        this.image_url = image_url;
+        this.description = description;
+        this.restriction = restriction;
         this.film_duration = film_duration;
         this.film_date = film_date;
+        this.countries = countries;
+        this.directors = directors;
+        this.genres = genres;
+        this.cinemas = cinemas;
+    }
+
+    public Films() {
     }
 
     public Long getFilm_id() {
@@ -32,12 +46,44 @@ public class Films {
         this.film_id = film_id;
     }
 
-    public String getFilm_name() {
-        return film_name;
+    public String getFilm_ru_name() {
+        return film_ru_name;
     }
 
-    public void setFilm_name(String film_name) {
-        this.film_name = film_name;
+    public void setFilm_ru_name(String film_ru_name) {
+        this.film_ru_name = film_ru_name;
+    }
+
+    public String getFilm_orig_name() {
+        return film_orig_name;
+    }
+
+    public void setFilm_orig_name(String film_orig_name) {
+        this.film_orig_name = film_orig_name;
+    }
+
+    public String getImage_url() {
+        return image_url;
+    }
+
+    public void setImage_url(String image_url) {
+        this.image_url = image_url;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRestriction() {
+        return restriction;
+    }
+
+    public void setRestriction(int restriction) {
+        this.restriction = restriction;
     }
 
     public Time getFilm_duration() {
@@ -54,5 +100,37 @@ public class Films {
 
     public void setFilm_date(Date film_date) {
         this.film_date = film_date;
+    }
+
+    public List<Countries> getCountries() {
+        return countries;
+    }
+
+    public void setCountries(List<Countries> countries) {
+        this.countries = countries;
+    }
+
+    public List<Directors> getDirectors() {
+        return directors;
+    }
+
+    public void setDirectors(List<Directors> directors) {
+        this.directors = directors;
+    }
+
+    public List<Genres> getGenres() {
+        return genres;
+    }
+
+    public void setGenres(List<Genres> genres) {
+        this.genres = genres;
+    }
+
+    public List<Cinemas> getCinemas() {
+        return cinemas;
+    }
+
+    public void setCinemas(List<Cinemas> cinemas) {
+        this.cinemas = cinemas;
     }
 }
