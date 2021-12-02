28159771ad5fe3307d7712a4c79af7376df11b34
Sociopath <sultan.narys2709@gmail.com>
2021-12-03 00:26:07 +0600

    add json parser

--- app/src/main/java/com/example/project/tab_layout/TodayFragment.java
+++ app/src/main/java/com/example/project/tab_layout/TodayFragment.java
@@ -19,10 +19,18 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.project.Activities.DetailsFilmActivity;
+import com.example.project.Entities.Cinemas;
+import com.example.project.Entities.Countries;
+import com.example.project.Entities.Directors;
 import com.example.project.Entities.Films;
+import com.example.project.Entities.Genres;
 import com.example.project.R;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -32,6 +40,8 @@ import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.Date;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -73,67 +83,14 @@ public class TodayFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_today, container, false);
 
-        int total = 9;
+        List<Films> films = HttpRequest();
+        int total = films.size();
         int column = 3;
         int row = total/column;
 
         GridLayout grid = view.findViewById(R.id.grid);
         grid.setColumnCount(column);
         grid.setRowCount(row + 1);
-
-        URL url = null;
-        BufferedReader reader = null;
-        try {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-
-            //ip ---------------------------------------------
-            url = new URL("http://:8080/api/allFilms");
-            HttpURLConnection connection = null;
-            try {
-                connection = (HttpURLConnection)url.openConnection();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            connection.setRequestMethod("GET");
-            connection.connect();
-            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
-                // все ок
-                InputStream inputStream = connection.getInputStream();
-                reader = new BufferedReader(new InputStreamReader(inputStream));
-
-                StringBuffer buffer = new StringBuffer();
-                String line = "";
-
-                while ((line = reader.readLine())!= null){
-                    buffer.append(line);
-                }
-                try {
-
-                    Type listType = new TypeToken<ArrayList<Films>>(){}.getType();
-                    List<Films> items = gson.fromJson(String.valueOf(buffer), listType);
-                    String film_name = items.get(0).getFilm_name();
-                    System.out.println("----------------------------------------------\n");
-                    System.out.println("Item: " + film_name);
-                    System.out.println("\n----------------------------------------------");
-
-                } catch (Throwable t) {
-                    System.out.println("///////////////////////////////////////////////////////////////");
-                    Log.e("My App", "Could not parse malformed JSON: \"" + buffer + "\"");
-                }
-            } else {
-                // ошибка
-                System.out.println("########    ######       ######         ######      ######");
-                System.out.println("##          ##    ##     ##    ##     ##      ##    ##    ##");
-                System.out.println("########    ######       ######       ##      ##    ######");
-                System.out.println("##          ##    ##     ##    ##     ##      ##    ##    ##");
-                System.out.println("########    ##     ##    ##     ##      ######      ##     ##");
-            }
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         for (int i = 0, c = 0, r = 0; i < total; i++, c++){
             if (c == column){
@@ -160,10 +117,12 @@ public class TodayFragment extends Fragment {
             LinearLayout layout = new LinearLayout(this.getContext());
             LinearLayout.LayoutParams params_layout = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
             layout.setOrientation(LinearLayout.VERTICAL);
+            int finalI = i;
             layout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Intent intent = new Intent(TodayFragment.super.getContext(), DetailsFilmActivity.class);
+                    //intent.putExtra("films", films.get(finalI));
                     startActivity(intent);
                 }
             });
@@ -197,5 +156,145 @@ public class TodayFragment extends Fragment {
             grid.addView(card);
         }
         return view;
+    }
+
+    public static List<Films> HttpRequest() {
+
+        URL url = null;
+        BufferedReader reader = null;
+        List<Films> films = new ArrayList<>();
+        try {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+
+            //ip ---------------------------------------------
+            url = new URL("http://192.168.137.1:8080/api/allFilms");
+            HttpURLConnection connection = null;
+            try {
+                connection = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            connection.setRequestMethod("GET");
+            connection.connect();
+
+            if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                // все ок
+                InputStream inputStream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line).append("\n");
+                }
+                System.out.println(buffer);
+                JSONArray jsonarray = new JSONArray(String.valueOf(buffer));
+
+                Long id = null;
+                String ru_name = null;
+                String orig_name = null;
+                String image_url = null;
+                String description = null;
+                int restriction = Integer.parseInt(null);
+                String duration = null;
+                String date = null;
+                List<Countries> countries = new ArrayList<>();
+                List<Directors> directors = new ArrayList<>();
+                List<Genres> genres = new ArrayList<>();
+                List<Cinemas> cinemas = new ArrayList<>();
+
+                JSONArray countries_json = null;
+                Long country_id = null;
+                String country_name = null;
+
+                JSONArray directors_json = null;
+                Long director_id = null;
+                String full_name = null;
+
+                JSONArray genre_json = null;
+                Long genre_id = null;
+                String genre_name = null;
+
+                JSONArray cinema_json = null;
+                Long cinema_id = null;
+                String ciname_name = null;
+                String ciname_address = null;
+
+                JSONArray cities_json = null;
+                Long city_id = null;
+                String city_name = null;
+
+                for (int i = 0; i < jsonarray.length(); i++) {
+                    JSONObject object = jsonarray.getJSONObject(i);
+
+                    id = object.getLong("film_id");
+                    ru_name = object.getString("film_ru_name");
+                    orig_name = object.getString("film_orig_name");
+                    description = object.getString("description");
+                    image_url = object.getString("image_url");
+                    restriction = object.getInt("restriction");
+                    duration = object.getString("film_duration");
+                    date = object.getString("film_date");
+
+                    countries_json = object.optJSONArray("countries");
+                    for (int l = 0; l < countries_json.length(); l++) {
+                        JSONObject country = countries_json.getJSONObject(l);
+                        country_id = country.getLong("country_id");
+                        System.out.println("country_id: " + country_id);
+                        country_name = country.getString("country_name");
+                        countries.add(new Countries(country_id, country_name));
+                    }
+
+                    directors_json = object.optJSONArray("directors");
+                    for (int j = 0; j < directors_json.length(); j++) {
+                        JSONObject director = directors_json.getJSONObject(j);
+                        director_id = director.getLong("director_id");
+                        full_name = director.getString("full_name");
+                        directors.add(new Directors(director_id, full_name));
+                    }
+
+                    genre_json = object.optJSONArray("genres");
+                    for (int j = 0; j < genre_json.length(); j++) {
+                        JSONObject genre = genre_json.getJSONObject(j);
+                        genre_id = genre.getLong("genre_id");
+                        genre_name = genre.getString("genre_name");
+                        genres.add(new Genres(genre_id, genre_name));
+                    }
+
+                    cinema_json = object.optJSONArray("cinema");
+                    for (int j = 0; j < cinema_json.length(); j++) {
+                        JSONObject cinema = cinema_json.getJSONObject(j);
+                        cinema_id = cinema.getLong("cinema_id");
+                        ciname_name = cinema.getString("cinema_name");
+                        ciname_address = cinema.getString("cinema_address");
+                    }
+//                    Cities city_entity = null;
+//                        cities_json = cinema_json.optJSONArray(0);
+//                        city_id = cities_json.getLong("city_id");
+//                        city_name = cities_json.getString("city_name");
+//                        city_entity = new Cities(city_id, city_name);
+//                    Cinemas cinema = new Cinemas(cinema_id, ciname_name, ciname_address, city_entity);
+
+                    films.add(new Films(id, ru_name, orig_name, image_url, description, restriction, Time.valueOf(duration), Date.valueOf(date), countries, directors, genres, cinemas));
+                }
+
+                System.out.println("Films: " + films.get(0).getFilm_ru_name());
+
+
+            } else {
+                // ошибка
+                System.out.println("########    ######       ######         ######      ######");
+                System.out.println("##          ##    ##     ##    ##     ##      ##    ##    ##");
+                System.out.println("########    ######       ######       ##      ##    ######");
+                System.out.println("##          ##    ##     ##    ##     ##      ##    ##    ##");
+                System.out.println("########    ##     ##    ##     ##      ######      ##     ##");
+            }
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
+        return films;
     }
 }
