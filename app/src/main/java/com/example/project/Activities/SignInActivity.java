fbf6cbe065a57b575132803714d0e0f7bbbbb1a5
Sultan Narys <sultan.narys2709@gmail.com>
2021-12-13 16:26:23 +0600

    add some methods

--- app/src/main/java/com/example/project/Activities/SignInActivity.java
+++ app/src/main/java/com/example/project/Activities/SignInActivity.java
@@ -1,7 +1,9 @@
 package com.example.project.Activities;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,16 @@ import com.example.project.R;
 
 public class SignInActivity extends AppCompatActivity {
 
-
     @SuppressLint("ResourceAsColor")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
+
+    }
+
+    public void toMainActivity(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
